a128bc7a3c84b8a5fc4db9f8319ce7dc055cadd1
andrebttleal <andrebttleal@gmail.com>
2021-10-17 18:44:21 -0300

    Commit

--- core/urls.py
+++ core/urls.py
@@ -13,9 +13,9 @@ from django.conf.urls import url
 urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path("", include("apps.authentication.urls")), # Auth routes - login / register
-    path("", include("apps.app.urls"))             # UI Kits Html files
+    path("", include("apps.app.urls")),             # UI Kits Html files
     
     # INCLUIDO MANUALMENTE
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}) 
 ]
